499b3b598ed87ea8452b087a6170a88b9b858089
Felipe Andrade <felipesgandrade@gmail.com>
2019-03-20 21:22:28 -0300

    upgrade unity version

---
+++ Assets/Resources/Scripts/ResourceManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType {
+	WOOD
+}
+
+public class ResourceManager : MonoBehaviour {
+
+	private Dictionary<ResourceType, int> amounts = new Dictionary<ResourceType, int>();
+	private Dictionary<ResourceType, 
+
+	public 
+}
