4b42ea594497637de59e40e73dd5d0613a603582
Felipe Andrade <felipesgandrade@gmail.com>
2019-03-20 23:19:37 -0300

    feat(resource): add UI

--- Assets/Resources/Scripts/ResourceManager.cs
+++ Assets/Resources/Scripts/ResourceManager.cs
@@ -6,10 +6,28 @@ public enum ResourceType {
 	WOOD
 }
 
-public class ResourceManager : MonoBehaviour {
+public static class ResourceManager {
 
-	private Dictionary<ResourceType, int> amounts = new Dictionary<ResourceType, int>();
-	// private Dictionary<ResourceType, 
+	private static Dictionary<ResourceType, int> amounts = new Dictionary<ResourceType, int>();
 
-	// public 
+	public static void AddResource(ResourceType type, int amount) {
+		amounts[type] = GetAmount(type) + amount;
+	}
+
+	public static bool RemoveResource(ResourceType type, int amount) {
+		if (GetAmount(type) < amount) {
+			return false;
+		}
+
+		amounts[type] = GetAmount(type) - amount;
+		return true;
+	}
+
+	public static int GetAmount(ResourceType type) {
+		if (!amounts.ContainsKey(type)) {
+			amounts[type] = 0;
+		}
+
+		return amounts[type];
+	}
 }
